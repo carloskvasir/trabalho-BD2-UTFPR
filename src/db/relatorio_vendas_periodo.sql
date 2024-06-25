369a05814c55cf8774e7a6970ea7f6393dcb5fbb
David Pereira <davidmarlonpereira@gmail.com>
2024-06-24 22:28:41 -0300

    create groups and users

--- src/db/relatorio_vendas_periodo.sql
+++ src/db/relatorio_vendas_periodo.sql
@@ -1,3 +1,29 @@
+-- 1. Criar roles
+CREATE ROLE admin_role;
+CREATE ROLE gerente_role;
+CREATE ROLE vendedor_role;
+
+-- 2. Criar usuários e atribuir roles
+CREATE USER gerente WITH PASSWORD 'gerente';
+CREATE USER vendedor WITH PASSWORD 'vendedor';
+
+GRANT admin_role TO postgres;
+GRANT gerente_role TO gerente;
+GRANT vendedor_role TO vendedor;
+
+-- Inserção de novos funcionários calculando o próximo valor de fun_codigo
+INSERT INTO tb_funcionarios (fun_codigo, fun_user, fun_nome, fun_cpf, fun_senha, fun_funcao)
+VALUES (
+           (SELECT COALESCE(MAX(fun_codigo), 0) + 1 FROM tb_funcionarios),
+           'gerente', 'Gerente', '12345678901', 'gerente', 'Gerente'
+       ),
+       (
+           (SELECT COALESCE(MAX(fun_codigo), 0) + 2 FROM tb_funcionarios),
+           'vendedor', 'Vendedor', '12345678902', 'vendedor', 'Vendedor'
+       );
+
+
+-- Criar a view para listar vendas
 CREATE OR REPLACE VIEW vw_listagem_vendas AS
 SELECT
     v.ven_codigo,
@@ -20,6 +46,14 @@ FROM
 ORDER BY
     v.ven_horario;
 
+-- Remover todas as permissões da view
+REVOKE ALL ON vw_listagem_vendas FROM PUBLIC;
+
+-- Conceder permissão de SELECT na view ao role gerente e admin
+GRANT SELECT ON vw_listagem_vendas TO gerente_role;
+GRANT SELECT ON vw_listagem_vendas TO admin_role;
+
+----- relatorio
 CREATE OR REPLACE FUNCTION relatorio_vendas_periodo(
     p_data_inicio DATE,
     p_data_fim DATE
